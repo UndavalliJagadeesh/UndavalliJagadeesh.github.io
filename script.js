793274499308eb840a1b416bee0815bb8d140c5f
UndavalliJagadeesh <20pa1a05h6@vishnu.edu.in>
2024-06-03 11:51:29 +0530

    Beautified existing code

--- script.js
+++ script.js
@@ -7,33 +7,33 @@ document.addEventListener('scroll', function () {
   if(window.innerWidth>480 && window.innerWidth<=768){
     maxLetterSpacing = 0.5;
     sensibility = 9000;
-  }else if(window.innerWidth>768){
+}else if(window.innerWidth>768){
     maxLetterSpacing = 0.7;
     sensibility = 4000;
-  }
-  var spacing = Math.min(maxLetterSpacing, scrollPos / sensibility); 
-  headerTitle.style.letterSpacing = spacing + 'em';
+}
+var spacing = Math.min(maxLetterSpacing, scrollPos / sensibility); 
+headerTitle.style.letterSpacing = spacing + 'em';
 });
 
 
 // function to update the clock in header for every second
 function updateClock() {
-      var now = new Date();
-      var hours = now.getHours().toString().padStart(2, '0');
-      var minutes = now.getMinutes().toString().padStart(2, '0');
-      var seconds = now.getSeconds().toString().padStart(2, '0');
-      document.getElementById('clock').textContent = hours + ':' + minutes + ':' + seconds;
-    }
+  var now = new Date();
+  var hours = now.getHours().toString().padStart(2, '0');
+  var minutes = now.getMinutes().toString().padStart(2, '0');
+  var seconds = now.getSeconds().toString().padStart(2, '0');
+  document.getElementById('clock').textContent = hours + ':' + minutes + ':' + seconds;
+}
 
 // function to display the details of project when clicked
-    function toggleDetails(tile) {
-      tile.classList.toggle('expanded');
-    }
+function toggleDetails(tile) {
+  tile.classList.toggle('expanded');
+}
 
 
 // calling the functions
-    setInterval(updateClock, 1000);
-    updateClock();
+setInterval(updateClock, 1000);
+updateClock();
 
 function showSkills(element) {
     const skillsContent = element.querySelector('.skills-content');
